c7113655004bf51f736a36a7c2d0f0c8def7d5b9
Sandhya-4931 <sandhya4931m@gmail.com>
2024-08-09 22:16:30 +0530

    preventDefault()

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -1,37 +1,36 @@
-import React from "react";
-
+import React from 'react';
 
 function Person() {
   
-function detail() {
-  const personalDetail = {
-    firstname: document.getElementById('first-name').value, 
-    lastname: document.getElementById('last-name').value, 
-    dob:document.getElementById('dob').value,
-    age:document.getElementById('age').value,
-}
-alert('Hello, I am '+ personalDetail.firstname + ' ' + personalDetail.lastname + '. I was born on ' + personalDetail.dob + ' and I am ' + personalDetail.age + ' years old');
-};
+  function detail(event) {
+    event.preventDefault(); // Prevents form submission and page refresh
+    const personalDetail = {
+      firstname: document.getElementById('first-name').value, 
+      lastname: document.getElementById('last-name').value, 
+      dob: document.getElementById('dob').value,
+      age: document.getElementById('age').value,
+    }
+    alert('Hello, I am ' + personalDetail.firstname + ' ' + personalDetail.lastname + '. I was born on ' + personalDetail.dob + ' and I am ' + personalDetail.age + ' years old');
+  };
 
-
-
-
-    return(
-      <>
+  return(
+    <>
       <div align="center">
-      <h1>PERSONAL DETAILS</h1>
-      <label>First name</label>
-      <input type="text" id='first-name' name="first-name"/>
-      <label>Last name</label>
-      <input type="text" id='last-name' name="last-name"/>
-      <label>DOB</label>
-      <input type="date" id='dob' name="dob"/>
-      <label>Age</label>
-      <input type="age" id="age" name="age"/>
-      <input type="button" value="Sign-in" onClick={detail()}/>
+        <form>
+          <h1>PERSONAL DETAILS</h1>
+          <label>First name</label>
+          <input type="text" id='first-name' name="first-name"/><br/>
+          <label>Last name</label>
+          <input type="text" id='last-name' name="last-name"/><br/>
+          <label>DOB</label>
+          <input type="date" id='dob' name="dob"/><br/>
+          <label>Age</label>
+          <input type="number" id="age" name="age"/><br/>
+          <button onClick={detail}>Sign in</button>
+        </form>
       </div>
-      </>
-    )
+    </>
+  )
 };
 
 export default Person;
